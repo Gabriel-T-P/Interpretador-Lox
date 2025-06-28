1b8ffeb396467bff43c3a84567cc24669591aeb2
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-06-28 09:48:07 -0300

    Fazendo a conexão do lox com o interpretador criado
    Co-authored-by: LeandroLisboaMatos <leandro.matos@discente.ufma.br>

--- src/main/java/com/eng/Interpreter.java
+++ src/main/java/com/eng/Interpreter.java
@@ -2,6 +2,15 @@ package com.eng;
 
 class Interpreter implements Expr.Visitor<Object> {
 
+  void interpret(Expr expression) {
+    try {
+      Object value = evaluate(expression);
+      System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
+
   private Object evaluate(Expr expr) {
     return expr.accept(this);
   }
@@ -109,6 +118,21 @@ class Interpreter implements Expr.Visitor<Object> {
       return false;
 
     return a.equals(b);
+  }
+
+  private String stringify(Object object) {
+    if (object == null)
+      return "nil";
+
+    if (object instanceof Double) {
+      String text = object.toString();
+      if (text.endsWith(".0")) {
+        text = text.substring(0, text.length() - 2);
+      }
+      return text;
+    }
+
+    return object.toString();
   }
 
 }
