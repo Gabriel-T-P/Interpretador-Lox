ccba12b2b19c334f338e8d136e3a122663170cc5
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-07-06 10:05:51 -0300

    adicioando suporte para armazenamento de variáveis no parser e sincronizado com o Run
    Co-authored-by: LeandroLisboaMatos <leandro.matos@discente.ufma.br>

--- src/main/java/com/eng/Interpreter.java
+++ src/main/java/com/eng/Interpreter.java
@@ -5,6 +5,8 @@ import java.util.List;
 class Interpreter implements Expr.Visitor<Object>,
     Stmt.Visitor<Void> {
 
+  private Environment environment = new Environment();
+
   void interpret(List<Stmt> statements) {
     try {
       for (Stmt statement : statements) {
@@ -26,6 +28,17 @@ class Interpreter implements Expr.Visitor<Object>,
   @Override
   public Void visitExpressionStmt(Stmt.Expression stmt) {
     evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
     return null;
   }
 
@@ -108,6 +121,11 @@ class Interpreter implements Expr.Visitor<Object>,
 
     // Unreachable.
     return null;
+  }
+
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
   }
 
   private void checkNumberOperand(Token operator, Object operand) {
