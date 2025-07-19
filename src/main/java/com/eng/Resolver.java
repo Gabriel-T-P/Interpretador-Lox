ca5a33e055a53f1d3ba0fa2ef8f597453435f971
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-07-19 10:15:43 -0300

    concluído capítulo 11.5 - Resolution Errors
    Co-authored-by: LeandroLisboaMatos <leandro.matos@discente.ufma.br>

--- src/main/java/com/eng/Resolver.java
+++ src/main/java/com/eng/Resolver.java
@@ -8,9 +8,15 @@ import java.util.Stack;
 class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private final Interpreter interpreter;
   private final Stack<Map<String, Boolean>> scopes = new Stack<>();
+  private FunctionType currentFunction = FunctionType.NONE;
 
   Resolver(Interpreter interpreter) {
     this.interpreter = interpreter;
+  }
+
+  private enum FunctionType {
+    NONE,
+    FUNCTION
   }
 
   void resolve(List<Stmt> statements) {
@@ -19,7 +25,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
   }
 
-  private void resolveFunction(Stmt.Function function) {
+  private void resolveFunction(
+      Stmt.Function function, FunctionType type) {
+    FunctionType enclosingFunction = currentFunction;
+    currentFunction = type;
+
     beginScope();
     for (Token param : function.params) {
       declare(param);
@@ -27,6 +37,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
     resolve(function.body);
     endScope();
+    currentFunction = enclosingFunction;
   }
 
   @Override
@@ -48,7 +59,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     declare(stmt.name);
     define(stmt.name);
 
-    resolveFunction(stmt);
+    resolveFunction(stmt, FunctionType.FUNCTION);
     return null;
   }
 
@@ -69,6 +80,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitReturnStmt(Stmt.Return stmt) {
+    if (currentFunction == FunctionType.NONE) {
+      Lox.error(stmt.keyword, "Can't return from top-level code.");
+    }
+
     if (stmt.value != null) {
       resolve(stmt.value);
     }
@@ -175,6 +190,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
       return;
 
     Map<String, Boolean> scope = scopes.peek();
+    if (scope.containsKey(name.lexeme)) {
+      Lox.error(name,
+          "Already a variable with this name in this scope.");
+    }
+
     scope.put(name.lexeme, false);
   }
 
