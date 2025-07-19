db4d88fc5513aed2197027d64f03ae609f636d92
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-07-19 10:38:57 -0300

    adicionado métodos em classes e propriedades de instâncias de classes
    Co-authored-by: LeandroLisboaMatos <leandro.matos@discente.ufma.br>

--- src/main/java/com/eng/Interpreter.java
+++ src/main/java/com/eng/Interpreter.java
@@ -77,7 +77,14 @@ class Interpreter implements Expr.Visitor<Object>,
   @Override
   public Void visitClassStmt(Stmt.Class stmt) {
     environment.define(stmt.name.lexeme, null);
-    LoxClass klass = new LoxClass(stmt.name.lexeme);
+
+    Map<String, LoxFunction> methods = new HashMap<>();
+    for (Stmt.Function method : stmt.methods) {
+      LoxFunction function = new LoxFunction(method, environment);
+      methods.put(method.name.lexeme, function);
+    }
+
+    LoxClass klass = new LoxClass(stmt.name.lexeme, methods);
     environment.assign(stmt.name, klass);
     return null;
   }
@@ -228,6 +235,17 @@ class Interpreter implements Expr.Visitor<Object>,
   }
 
   @Override
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (object instanceof LoxInstance) {
+      return ((LoxInstance) object).get(expr.name);
+    }
+
+    throw new RuntimeError(expr.name,
+        "Only instances have properties.");
+  }
+
+  @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
   }
@@ -245,6 +263,20 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     return evaluate(expr.right);
+  }
+
+  @Override
+  public Object visitSetExpr(Expr.Set expr) {
+    Object object = evaluate(expr.object);
+
+    if (!(object instanceof LoxInstance)) {
+      throw new RuntimeError(expr.name,
+          "Only instances have fields.");
+    }
+
+    Object value = evaluate(expr.value);
+    ((LoxInstance) object).set(expr.name, value);
+    return value;
   }
 
   @Override
