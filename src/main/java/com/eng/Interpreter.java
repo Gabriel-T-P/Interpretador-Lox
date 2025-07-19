6e05a3cb27bd3d5436f8adc2ceaac4b777754bb2
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-07-19 14:52:21 -0300

    adicionado concatenação entre string e double

--- src/main/java/com/eng/Interpreter.java
+++ src/main/java/com/eng/Interpreter.java
@@ -193,12 +193,12 @@ class Interpreter implements Expr.Visitor<Object>,
           return (double) left + (double) right;
         }
 
-        if (left instanceof String && right instanceof String) {
-          return (String) left + (String) right;
+        if (left instanceof String || right instanceof String) {
+          return stringify(left) + stringify(right);
         }
 
         throw new RuntimeError(expr.operator,
-            "Operands must be two numbers or two strings.");
+            "Operands must be two numbers or at least one must be a string.");
       case SLASH:
         checkNumberOperands(expr.operator, left, right);
         return (double) left / (double) right;
